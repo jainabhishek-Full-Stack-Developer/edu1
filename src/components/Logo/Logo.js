dc78560a38dc350a8626d47a4616b4a0a1043ae6
jainabhishek-Full-Stack-Developer <133668364+jainabhishek-Full-Stack-Developer@users.noreply.github.com>
2025-02-11 18:37:27 +0530

    five commit

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,10 +2,10 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 const Logo = () => {
-    const adminUrl = process.env.NEXT_PUBLIC_ADMIN_URL;
+
     return (
         <div className={styles.logo}>
-            <Link href={adminUrl} aria-label="CourseHive Home">
+            <Link href="/" aria-label="CourseHive Home">
                 <span>CourseHive</span>
             </Link>
         </div>
